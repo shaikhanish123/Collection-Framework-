63b13e99c625227bad7d020cd2160dee22ce4a47
Anish <anishahamadf@gamil.com>
2026-04-29 11:50:31 +0530

    added something

---
+++ HashsetMethod/HashSetExample.java
@@ -0,0 +1,10 @@
+package HashsetMethod;
+
+import java.util.HashSet;
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        HashSet<String> hs = new HashSet<String>();
+        hs.add("akash");
+    }
+}
