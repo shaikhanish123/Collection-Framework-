641a0ced6c1bdc0af34a0ce1b4d8394efe94e760
Anish <anishahamadf@gamil.com>
2026-04-30 12:07:18 +0530

    added some method

--- HashsetMethod/HashSetExample.java
+++ HashsetMethod/HashSetExample.java
@@ -1,10 +1,24 @@
 package HashsetMethod;
-
 import java.util.HashSet;
 
 public class HashSetExample {
     public static void main(String[] args) {
         HashSet<String> hs = new HashSet<String>();
-        hs.add("akash");
-    }
+       hs.add("akash");
+       hs.add("kaveri");
+       hs.add("akash");
+         System.out.println("before clone the hashet:"+hs);
+        // hs.clear();
+        //clone() method
+        // HashSet<String> hs1=(HashSet<String>)hs.clone();
+        // System.out.println("after clone:"+hs1);
+        System.out.println(hs.contains("kaveri"));
+
+        // System.out.println("after clear:"+hs);
+
+
+    
+   
+
+}
 }
---
+++ HashsetMethod/TreeSetMethod.java
@@ -0,0 +1,10 @@
+package HashsetMethod;
+
+public class TreeSetMethod {
+    public static void main(String[] args) {
+    
+
+        
+    }
+    
+}
